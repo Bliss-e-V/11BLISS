48f5993f94dd2d000be0dbdf7125fb52efdd1adb
Julian M. Kleber <julian.kleber@sail.black>
2024-04-05 18:27:26 +0200

    initialized frontend with form starter

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 11BLISS
 HackHPI Repo of Team 3 
+
+# Frontend
+
+```bash
+npm install -D tailwindcss postcss autoprefixer
+```
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+
+let inputValue = ''; // Initialize a variable to hold the input value
+
+// Function to handle form submission
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  var address = document.getElementsByName('address')[0].value
+  var encodedAddress = encodeURIComponent(address)
+  console.log(encodedAddress)
+  sleep(1000)
+  
+  alert('Submitted value: ' + inputValue); // For demonstration purposes, alert the input value
+  // .then(response => response.json())
+  // fetch('http://localhost:5000/geocoding?address=' + encodedAddress)
+  // .then(data => {
+  //     console.log(data)
+  // })
+
+}
+
+</script>
+
+<form method="POST"  on:submit={handleSubmit}   >
+	<label>
+		Address
+		<input name="address" type="text" bind:value={inputValue}>
+	</label>
+	<button type="submit">Search</button>
+</form>
+
+<p>{inputValue}</p>
